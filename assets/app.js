fcbd39e63b03ee2bec6eb5130fec56b7a98c8281
Seth Bowman <brandonsethbowman@gmail.com>
2021-06-23 12:26:46 -0500

    Completed exercise 1 and 2

--- assets/app.js
+++ assets/app.js
@@ -46,21 +46,86 @@
 // seth.codeSome();
 
 
-class Person 
-{
-constructor (firstName, lastName, favoriteColor) 
-{
+// class Person 
+// {
+// constructor (firstName, lastName, favoriteColor) 
+// {
     
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.favoriteColor = favoriteColor;   
-}
-details() 
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.favoriteColor = favoriteColor;   
+// }
+// details() 
+//     {
+//         alert(`Your name is ${this.firstName} ${this.lastName} and your favorite color is ${this.favoriteColor}!`);
+//     }      
+// }
+
+// let seth = new Person("Seth", "Bowman", "Red");
+
+// seth.details();
+
+
+// let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// colors.length // the numer of values in the array (7)
+
+// colors[3] // would access the color 'green' in the array (red would be  0)
+
+// colors.push("burlywood"); // adds a new value to the end of the array
+
+// colors.pop(); // removes the last item in the array
+
+// colors.unshift("burlywood"); // add a value to the front of the array
+
+// colors.shift(); // removes the first value of the array
+
+
+// for(let i = 0; i < colors.length; i++) 
+// {
+//     console.log(`${colors[i]} is ${colors[i].length} characters long.`);
+// }
+
+
+// colors.forEach(function (value) 
+// {
+//     console.log(`${value} is ${value.length} characters long.`);
+// });
+
+
+// =======================================
+
+
+// Exerise one
+
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arraySum(numbers) 
+{
+    let sum = 0;
+
+    for(let i = 0; i < numbers.length; i++)
     {
-        alert(`Your name is ${this.firstName} ${this.lastName} and your favorite color is ${this.favoriteColor}!`);
-    }      
+        sum += numbers[i];
+    }
+    
+    return sum;
 }
 
-let seth = new Person("Seth", "Bowman", "Red");
+console.log(arraySum(numbers));
 
-seth.details();
+
+// Exercise two
+
+const book = {};
+book.title = "Lord Of The Rings";
+book.author = "J.R.R. Tolkien"
+book.pages = 1241;
+book.readCount = 5;
+
+book.info = () => 
+{
+    return `${book.title} by ${book.author}, has ${book.pages} pages. I have read this book ${book.readCount} times.`;
+}
+
+console.log(book.info());
